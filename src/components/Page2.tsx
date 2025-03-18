736d6f4b1b27a8d3353535745a38d5e58749ad55
Nattanant Saengpattanakornkit <nattanantsaengpattanakornkit@Urmac.local>
2025-03-19 01:50:04 +0700

    Add page background

---
+++ src/components/Page2.tsx
@@ -0,0 +1,8 @@
+export const Page2  = () => {
+    return (
+        <div className="flex flex-col items-center p-6 max-w-sm mx-auto my-auto bg-[url(/page2/background.webp)] bg-[auto_125%] bg-center rounded-xl shadow-md space-y-4 min-h-screen">
+            <img src="/page2/logo.webp" className="w-22 object-cover absolute top-13"/>
+            <div className="flex col-start">PHYSICAL SCIENCE</div>
+		</div>
+    )
+}
