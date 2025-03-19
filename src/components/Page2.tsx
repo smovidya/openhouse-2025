38528e6e21124ff5ddd930e639a554e7d2e51d7e
Nattanant Saengpattanakornkit <nattanantsaengpattanakornkit@Urmac.local>
2025-03-20 01:21:08 +0700

    Major header edited

--- src/components/Page2.tsx
+++ src/components/Page2.tsx
@@ -1,57 +1,64 @@
+import { Dot } from "lucide-react";
+
 export const Page2 = () => {
+	const styleMajor = "font-poppins italic text-[#002144] text-xs block text-center";
+    const styleGroup = "font-soloistextra text-sm text-white text-center p-2"
+
+	const scienceData = [
+		{
+			grouping: "/page2/PHYS.png",
+			majors: [
+				"Mathematics & Computer Science",
+				"Chemistry",
+				"BSAC",
+				"Physics",
+			],
+		},
+		{
+			grouping: "/page2/BIO.png",
+			majors: ["Biology", "Botany", "Biochemistry", "Microbiology"],
+		},
+		{
+			grouping: "/page2/TECH.png",
+			majors: [
+				"Chemical Technology",
+				"Materials Science",
+				"Imaging and Printing Technology",
+				"Food Technology",
+			],
+		},
+		{
+			grouping: "/page2/NATURE.png",
+			majors: ["Geology", "Marine Science", "Environmental Science"],
+		},
+        {
+            grouping: "/page2/SIS.png",
+			majors: ["BBTech", "BISTech"],
+        }
+	];
+
 	return (
-		<div className="flex flex-col items-center p-6 max-w-sm mx-auto my-auto bg-[url(/page2/background.webp)] bg-[auto_125%] bg-center rounded-xl shadow-md space-y-4 min-h-screen">
-			<img
-				src="/page2/logo.webp"
-				className="w-22 object-cover absolute top-13"
-			/>
-            <div className="absolute top-35 items-center justify-center">
+		<div className="bg-[url(/page2/new_bg_2.webp)] bg-cover min-h-screen">
+            <div className="h-40"></div>
+            <div className="flex flex-col justify-center items-center">
             <img
-                    src="/page2/middleLine.webp"
-                    className="w-3 object-cover fixed"
-                />
+                        src="/page2/SciCU.png"
+                        className="w-100"
+                    />     
+                <div className="w-full h-1/2">
+                    {scienceData.map((item) => (
+                        <div className="">
+                        <img src={item.grouping} className=""/>
+                        {item.majors.map((major) => (
+                            <div className="flex flex-row items-center justify-center" key={major}>
+                                <span className="text-sm">•</span>
+                                <div className={`${styleMajor}`}>{major}</div>
+                            </div>
+                            ))}
+                        </div>
+                        ))}
+                </div>
             </div>
-            <div className="flex flex-col justify-between absolute top-35 w-80 h-auto px-4 space-y-1">
-                {/* PHYS */}
-                <div className="w-1/2 text-left">
-                    <p className="font-soloistextra text-[1.2vw] text-white text-right px-1">PHYSICAL SCIENCE</p>
-                    <div className="flex flex-row items-start">
-                        <span className="text-lg">•</span>
-                        <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Mathematics & Computer Science</a>
-                    </div>
-                    <div className="flex flex-row items-start">
-                        <span className="text-lg">•</span>
-                        <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Chemistry</a>
-                    </div>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">BSAC</a>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Physics</a>
-                </div>
-                {/* BIO */}
-                <div className="w-1/2 text-left pl-40 -mt-3.5 ">
-                    <p className="font-soloistextra text-[1.2vw] text-white whitespace-nowrap">BIOLOGICAL SCIENCE</p>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Biology</a>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Botany</a>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Biochemistry</a>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Microbiology</a>
-                </div>
-                {/* TECH */}
-                <div className="w-1/2 text-left">
-                    <p className="font-soloistextra text-[1.2vw] text-white text-right">TECHNOLOGICAL SCIENCE</p>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Chemical Technology</a>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Materials Science</a>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Imaging and Printing Technology</a>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Food Technology</a>
-                </div>
-                {/* Nature */}
-                <div className="w-1/2 text-left pl-40 -mt-3.5 ">
-                    <p className="font-soloistextra text-[1.2vw] text-white whitespace-nowrap">NATURAL SCIENCE</p>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Geology</a>
-                    <a href="" className="font-poppins italic text-[#002144] text-[1vw] block">Marine Science</a>
-                    <a href="" className="font-poppins italic text-[#002144] whitespace-nowrap text-[1vw] block">Environmental Science</a>
-                </div>
-
-
-            </div>
-		</div>
+        </div>
 	);
 };
